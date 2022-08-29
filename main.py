d06d80ad0d4338d232307d242cab710bf1f5b104
felixtsu <humaeks@gmail.com>
2022-08-29 15:11:11 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-currentDisk: Sprite = None
-list1: List[Sprite] = []
-list2: List[Sprite] = []
-list3: List[Sprite] = []
-
-def getDiskLength(disk:Sprite) :
-    return sprites.read_data_number(disk, "length")
-
 def setCurrentDisk(pos: number):
     global currentDisk
-    if not currentDisk:
+    if not (currentDisk):
         if pos == 0 and len(list1) > 0:
             currentDisk = list1.pop()
         elif pos == 1 and len(list2) > 0:
             currentDisk = list2.pop()
         elif pos == 2 and len(list3) > 0:
             currentDisk = list3.pop()
-
-
 def putDownDisk(disk2: Sprite, pos2: number):
-    global currentDisk
-    if not currentDisk:
+    if not (currentDisk):
         return False
     if pos2 == 0:
         if len(list1) == 0 or getDiskLength(disk2) < getDiskLength(list1[len(list1) - 1]):
@@ -28,15 +17,20 @@ def putDownDisk(disk2: Sprite, pos2: number):
         else:
             return False
     elif pos2 == 1:
-        if len(list2) == 0 or getDiskLength(disk2) < getDiskLength(list1[len(list2) - 1]):
+        if len(list2) == 0 or getDiskLength(disk2) < getDiskLength(list2[len(list2) - 1]):
             list2.append(disk2)
             return True
         else:
             return False
-    elif len(list3) == 0 or getDiskLength(disk2) < getDiskLength(list1[len(list3) - 1]):
+    elif len(list3) == 0 or getDiskLength(disk2) < getDiskLength(list3[len(list3) - 1]):
         list3.append(disk2)
         return True
     else:
         return False
-
+def getDiskLength(disk: Sprite):
+    return sprites.read_data_number(disk, "length")
+list3: List[Sprite] = []
+list2: List[Sprite] = []
+list1: List[Sprite] = []
+currentDisk: Sprite = None
 Hanoi.gamestart()
--- main.ts
+++ main.ts
@@ -1,11 +1,3 @@
-let currentDisk : Sprite = null
-let list1 : Sprite[] = []
-let list2 : Sprite[] = []
-let list3 : Sprite[] = []
-function getDiskLength(disk: Sprite): number {
-    return sprites.readDataNumber(disk, "length")
-}
-
 function setCurrentDisk(pos: number) {
     
     if (!currentDisk) {
@@ -22,7 +14,6 @@ function setCurrentDisk(pos: number) {
 }
 
 function putDownDisk(disk2: Sprite, pos2: number): boolean {
-    
     if (!currentDisk) {
         return false
     }
@@ -36,14 +27,14 @@ function putDownDisk(disk2: Sprite, pos2: number): boolean {
         }
         
     } else if (pos2 == 1) {
-        if (list2.length == 0 || getDiskLength(disk2) < getDiskLength(list1[list2.length - 1])) {
+        if (list2.length == 0 || getDiskLength(disk2) < getDiskLength(list2[list2.length - 1])) {
             list2.push(disk2)
             return true
         } else {
             return false
         }
         
-    } else if (list3.length == 0 || getDiskLength(disk2) < getDiskLength(list1[list3.length - 1])) {
+    } else if (list3.length == 0 || getDiskLength(disk2) < getDiskLength(list3[list3.length - 1])) {
         list3.push(disk2)
         return true
     } else {
@@ -52,4 +43,12 @@ function putDownDisk(disk2: Sprite, pos2: number): boolean {
     
 }
 
+function getDiskLength(disk: Sprite): number {
+    return sprites.readDataNumber(disk, "length")
+}
+
+let list3 : Sprite[] = []
+let list2 : Sprite[] = []
+let list1 : Sprite[] = []
+let currentDisk : Sprite = null
 Hanoi.gamestart()
